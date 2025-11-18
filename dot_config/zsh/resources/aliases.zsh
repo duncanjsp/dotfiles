7a6b12083d668baef9d4a3cc7f00f1af7145d411
Duncan Stevenson-Price <duncan@psmail.co>
2025-11-18 11:09:34 +0000

    Zed & Ohmyposh updates

--- dot_config/zsh/resources/aliases.zsh
+++ dot_config/zsh/resources/aliases.zsh
@@ -6,3 +6,7 @@ alias lt="eza -aTL 3 --icons"
 
 # bar is a better cat with syntax highlighting
 alias cat="bat"
+
+# Handy git aliases
+alias gwip='git add -A; git rm $(git ls-files --deleted) 2> /dev/null; git commit --no-verify --no-gpg-sign --message "--wip-- [skip ci]"'
+alias gunwip='git rev-list --max-count=1 --format="%s" HEAD | grep -q "\--wip--" && git reset HEAD~1'
